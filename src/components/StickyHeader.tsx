f80b843a696b032fc6c4fcf4cc453a87a8833293
Syed Afrid Ali <aliatif908@gmail.com>
2024-08-31 02:19:48 +0530

    added data for Work Experience and Academics fix: sticky header lag

--- src/components/StickyHeader.tsx
+++ src/components/StickyHeader.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { debounce } from "lodash";
 import { useEffect, useState } from "react";
 import Header from "./Header";
 import DesktopNav from "./DesktopNav";
@@ -8,23 +7,19 @@ import DesktopNav from "./DesktopNav";
 export default function StickyHeader() {
   const [shrunk, setShurnk] = useState<boolean>(false);
 
-  const handleScroll = debounce(
-    () => {
-      if (window.scrollY > 0) {
-        setShurnk(true);
-      } else {
-        setShurnk(false);
-      }
-    },
-    100,
-    { leading: true, trailing: true }
-  );
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setShurnk(true);
+    } else {
+      setShurnk(false);
+    }
+  };
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener("scroll", handleScroll.cancel);
+      window.removeEventListener("scroll", handleScroll);
     };
   });
 
