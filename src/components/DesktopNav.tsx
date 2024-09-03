67da61d760c6e379334790080a9a946c39e9091e
Syed Afrid Ali <aliatif908@gmail.com>
2024-09-03 15:47:28 +0530

    added middleware to protected routes that are not yet developed

--- src/components/DesktopNav.tsx
+++ src/components/DesktopNav.tsx
@@ -3,6 +3,8 @@ import { IRoute } from "@/interfaces/i-routes";
 import Link from "next/link";
 import LoginLogout from "./LoginLogout";
 
+const featureAvailable = false;
+
 const DesktopNav = ({ shrunk }: { shrunk: boolean }) => {
   return (
     <div
@@ -11,16 +13,18 @@ const DesktopNav = ({ shrunk }: { shrunk: boolean }) => {
       } container mx-auto md:flex hidden flex-wrap items-center p-4 md:p-0 md:mb-4 mb-5`}
     >
       <nav className="nav--desktop md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-        {routes.map((route: IRoute, $idx: number) => (
-          <Link
-            key={`route-${$idx}`}
-            href={route.path}
-            className="inline-block px-2 py-0.5 mr-3 text-app-secondary nav-links--desktop"
-          >
-            {route.menuName}
-          </Link>
-        ))}
-        <LoginLogout className="nav-links--desktop" />
+        {routes
+          .filter((route) => !route.off)
+          .map((route: IRoute, $idx: number) => (
+            <Link
+              key={`route-${$idx}`}
+              href={route.path}
+              className="inline-block px-2 py-0.5 mr-3 text-app-secondary nav-links--desktop"
+            >
+              {route.menuName}
+            </Link>
+          ))}
+        {featureAvailable && <LoginLogout className="nav-links--desktop" />}
       </nav>
     </div>
   );
